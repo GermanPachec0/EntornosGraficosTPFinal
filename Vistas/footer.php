c0f0296c7de9a5868ba47afb74e34ed836c832b6
German <germanpacheco98@gmail.com>
2022-06-01 21:08:05 -0300

    link_utn

--- Vistas/footer.php
+++ Vistas/footer.php
@@ -2,7 +2,7 @@
             <!-- Copyright -->
             <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
               © 2022 Copyright:
-              <a class="text-dark" href="https://utn.frro.etu.ar/">Facultad Regional De Rosario</a>
+              <a class="text-dark" href="https://www.frro.utn.edu.ar/">Facultad Regional De Rosario</a>
             </div>
             <!-- Copyright -->
           </footer>   
