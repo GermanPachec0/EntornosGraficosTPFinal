4f006118b5edd7363761d8c9135e7acbf495d019
JuliStriglio <102307488+JuliStriglio@users.noreply.github.com>
2022-06-07 14:25:26 -0300

    Add files via upload

---
+++ Vistas/menuAlumno.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Document</title>
+</head>
+<body style="background-color: grey;">
+    <div class="container">
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark  nav-justified" >
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/6/67/UTN_logo.jpg" alt="Avatar Logo" style="width:100px;" class="rounded-pill" > 
+              </a>
+            </div>
+            <li class="nav-item"><a class="nav-link" href="info">Informacion</a></li>
+            <li class="nav-item"><a class="nav-link" href="#services">Cerrar sesion</a></li>
+            <li class="nav-item"><a class="nav-link" href="#portfolio">Consultas</a></li>
+            <li class="nav-item"><a class="nav-link" href="#contact">Mis Consultas</a></li>
+          </nav>
