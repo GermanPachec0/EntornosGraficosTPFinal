f4e6946537c24f507b774d95fec67a030c4754ee
Fausto Saludas <fausaludas14@gmail.com>
2022-06-28 22:10:22 -0300

    main

--- Vistas/conexion.php
+++ Vistas/conexion.php
@@ -1,8 +1,8 @@
 <?php
         $hostname = "localhost";
-        $nombreUsuario = "root";
-        $password ="root";
-        $db = "bs_consultas";
+        $nombreUsuario = "id19186121_root";
+        $password ="Root12345678.";
+        $db = "id19186121_bs_consultas";
         $link = mysqli_connect($hostname,$nombreUsuario,$password) or die("Error al Conectar");
         mysqli_select_db($link,$db);
 ?>
