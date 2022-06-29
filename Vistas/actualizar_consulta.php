46233beeaf3b531cb396ae045ac368045e7f1416
German <germanpacheco98@gmail.com>
2022-06-28 22:30:29 -0300

    url_server

--- Vistas/actualizar_consulta.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once "conexion.php";
-include_once "consulta.php";
-$estudiante = new consulta($_POST["idMateria"], $_POST["legajoDocente"], $_POST["fechayhoraAlt"],$_POST["idConsulta"],$_POST["estado"],$_POST["enlaceZoom"],$_POST["cupo"]);
-$estudiante->actualizar();
-header("Location: listadoConsultasAdmin.php");
