6665aed86f72be06133cbcd0ba9c592b0ee2969b
Fausto Saludas <fausaludas14@gmail.com>
2022-08-20 14:03:15 -0300

    main

--- Vistas/template/mapaSitio.php
+++ Vistas/template/mapaSitio.php
@@ -1,3 +1,28 @@
+<?php 
+  include("../usuario.php");
+  session_start();
+  if(isset($_SESSION['usuario']))
+  {
+      $myUser= new Usuario();
+      $myUser = $_SESSION['usuario'];
+    if($myUser -> getTipoUsuario() == "administrador")
+    {
+      include("menuAdmin.php");
+    }
+    else if ($myUser -> getTipoUsuario() == "docente")
+    {
+    include("menuDocente.php");
+    }
+    else if ($myUser -> getTipoUsuario() == "alumno")
+    {
+    include("menuAlumno.php");
+    }
+  } 
+  else{
+    include("menuVisitante.php");
+  }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,29 +34,19 @@
     <link href="estilos/signin.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     
-    <title>Gestion consultas</title>
+    <title>Mapa de Sitio</title>
 </head>
 <body style="background-color: grey;">
 
 <?php $url ="http://localhost/EntornosGraficosTPFinal"?>
 
-    <div class="container ">
-        <nav class="navbar navbar-expand-sm bg-dark navbar-dark  nav-justified" >
-            <div class="container-fluid">
-              <a class="navbar-brand" href="<?php echo $url?>/index.php">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/6/67/UTN_logo.jpg" alt="Avatar Logo" style="width:100px;" class="rounded-pill" > 
-              </a>
-            </div>
-            
-            <li class="nav-item"><a class="nav-link" href="<?php echo $url?>/Vistas/Informacion.php">Informacion</a></li>
-            <li class="nav-item"><a class="nav-link" href="<?php echo $url?>/Vistas/Login.php">Iniciar Sesión</a></li>
-        
-          </nav>
+ 
 
-          <div class="list-group">
-  <button type="button" class="list-group-item list-group-item-action active" aria-current="true">
-  Mapa del sitio
-  </button>
+  <div class="mt-4 p-5 bg-primary text-white rounded text-center">
+            <h1 >Mapa del sitio</h1>
+        </div>
+
+  
   <div><br></div>
   <button type="button" class="list-group-item list-group-item-action active" aria-current="true">
   Alumno
@@ -62,5 +77,3 @@
   <a class="list-group-item list-group-item-action" href="<?php echo $url?>/index.php">Inicio</a>
   <a class="list-group-item list-group-item-action" href="<?php echo $url?>/Vistas/Informacion.php">Informacion</a>
   
-
-</div>
